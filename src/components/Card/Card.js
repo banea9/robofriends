886a268df118744d7be636370f9a5e37b0f4da09
banea9 <banea9@gmail.com>
2020-04-21 20:25:30 +0200

    adding size on imgs

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ id, name, email }) => {
   return (
     <div className="tc pointer br2 bg-light-green dib pa3 ma2 grow bw2 shadow-5 ">
-      <img src={`https://robohash.org/${id}`} alt="robot" />
+      <img src={`https://robohash.org/${id}?size=200x200`} alt="robot" />
       <h3>{name}</h3>
       <p>{email}</p>
     </div>
--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ onInputChange }) => {
   return (
     <div className="pa">
       <input
+        aria-label="Search robots"
         onChange={onInputChange}
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
